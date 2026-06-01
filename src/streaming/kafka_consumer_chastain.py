bb6ebe91aea3ceb7cc94870182438cf3f7dd8deb
Megan <megan.chastain@hotmail.com>
2026-05-31 20:04:40 -0500

    update

--- src/streaming/kafka_consumer_chastain.py
+++ src/streaming/kafka_consumer_chastain.py
@@ -17,11 +17,8 @@ Date: 2026-05
 
 Terminal command to run this file from the root project folder:
 
-    uv run python -m streaming.kafka_consumer_case
+    uv run python -m streaming.kafka_consumer_chastain
 
-OBS:
-  Don't edit this file - it should remain a working example.
-  Copy it, rename it consumer_yourname.py, and modify your copy.
 """
 
 # === DECLARE IMPORTS ===
